134aefda4be6990bace084f01e10ace078564bff
Cinetik <florian.meskens@gmail.com>
2015-04-30 12:57:07 +0200

    TODO Comment

--- app/game/game.service.js
+++ app/game/game.service.js
@@ -11,7 +11,12 @@
         var gameService =  {
             getGames: getGames,
         };
-
+        /* TODO Create a mock service that allows CRUD for games.
+        *	This will give us the possibility to add games with description, rules and other informations.
+        *	Since every game is different, we need to define a mechanism to make game management as flexible as 
+        *	possible. 
+        *	Also, we'll tweak a bit for popular games like CSGO, LoL, Dota, Hearthstone, ...
+        */
         function getGames() {
             return false;
         }
