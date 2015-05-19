0b8b50110a02201cf9dcf65290a1250ddd7b7ad7
Florian Meskens <florian.meskens@gmail.com>
2015-05-19 21:29:55 +0200

    Modified file structure to allow express to run, added first mongoose usage for db

---
+++ routes.js
@@ -0,0 +1,30 @@
+// app/routes.js
+
+// grab the nerd model we just created
+var Game = require('./models/game');
+
+   module.exports = function(app) {
+
+       // server routes ===========================================================
+       // handle things like api calls
+       // authentication routes
+
+       // sample api route
+       app.get('/api/game', function(req, res) {
+           // use mongoose to get all nerds in the database
+           Game.find(function(err, games) {
+
+               // if there is an error retrieving, send the error.
+                               // nothing after res.send(err) will execute
+               if (err)
+                   res.send(err);
+
+               res.json(games); // return all nerds in JSON format
+           });
+       });
+
+       // route to handle creating goes here (app.post)
+       // route to handle delete goes here (app.delete)
+
+       // frontend routes =========================================================
+   };
